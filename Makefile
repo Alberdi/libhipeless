cfe537666479749beedac79340f5be0fb48437a2
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2012-10-23 11:16:16 +0200

    Separates main() method from the library files.

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@
 #FLAGS = -O3 -L/usr/lib64/OpenCL/vendors/intel/ -lOpenCL -I/usr/local/cuda/include/
 FLAGS = -O3 -L /opt/AMDAPP/lib/x86_64/ -l OpenCL -I /opt/AMDAPP/include/
 
-vec_sum: matmul.cpp
-	mpic++ -o matmul.o matmul.cpp $(FLAGS)
+main: main.c
+	mpic++ -o main.o main.c libhipeless.cpp $(FLAGS)
 
 clean:
-	rm matmul.o
+	rm main.o
 
--- libhipeless.cpp
+++ libhipeless.cpp
@@ -192,38 +192,6 @@ int matrix_multiplication(cl_float *C, cl_float *A, cl_float *B, cl_uint rowsA, 
     
 }
 
-int main(int argc, char* argv[]) {
-  unsigned int flags = USE_GPU | USE_MPI;
-  int i, j;
-  int rowsA = 1024, colsA = 512, rowsB = 512, colsB = 2048;
-  cl_float *A, *B, *C;
-
-  A = (cl_float *) malloc(rowsA*colsA*sizeof(cl_float));
-  B = (cl_float *) malloc(rowsB*colsB*sizeof(cl_float));
-  C = (cl_float *) malloc(rowsA*colsB*sizeof(cl_float));
-
-  for(i=0;i<rowsA;i++)
-    for(j=0;j<colsA;j++)
-      A[i*colsA+j]=1;
-
-  for(i=0;i<rowsB;i++)
-    for(j=0;j<colsB;j++)
-      B[i*colsB+j] = i==j ? 1 : 0;
-
-  matrix_multiplication(C, A, B, rowsA, colsA, rowsB, colsB, flags, argc, argv);
-
-  // Result checking
-  float x = 0.0;
-  for(i=0; i<rowsA; i++) {
-    for(j=0; j<colsB; j++) {
-      x += C[i*colsB+j];
-    }
-  }
-
-  // TODO This check is not correct always
-  if(x==rowsA*colsA || x==rowsA*colsB) printf("CORRECTO (%f)\n", x);
-  else printf("INCORRECTO: %f (%d, %d)\n", x, rowsA*colsA, rowsA*colsB);
- 
-  //matrix_print(C, rowsA, colsB);
-
-}
+/*int main(int argc, char* argv[]) {
+  
+}*/
