fc091bb468c56651ef3c8093d1af5a6a1ecb84e9
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2013-02-01 13:43:03 +0100

    Changes to functions to match the blas interface.
    A lot more of parameters are coming into the OpenCL kernel, they
    will be useful for working with transposed matricces.

--- libhipeless.cpp
+++ libhipeless.cpp
@@ -21,8 +21,8 @@ const char* readKernelFromSource(const char* source) {
     return sourceString.c_str();
 }
 
-int opencl_operation(cl_char TransA, cl_char TransB, cl_int m, cl_int n, cl_int k, cl_float alpha, cl_float *a,
-                     cl_float *b, cl_float beta, cl_float *c, unsigned int flags, const char* kernelfunction) {
+int opencl_operation(cl_int nota, cl_int notb, cl_int m, cl_int n, cl_int k, cl_float alpha, cl_float *a, cl_int lda,
+                     cl_float *b, cl_int ldb, cl_float beta, cl_float *c, cl_int ldc, unsigned int flags, const char* kernelfunction) {
   int i;
   cl_uint num_devices;
   cl_int errcode;
@@ -107,30 +107,20 @@ int opencl_operation(cl_char TransA, cl_char TransB, cl_int m, cl_int n, cl_int 
       errcode = clEnqueueWriteBuffer(command_queues[i], memC[i], CL_TRUE, 0, iter_m*n*sizeof(cl_float), &c[i*dev_m*n], 0, NULL, NULL);
       checkErr(errcode, "clEnqueueWriteBufferC");
     }
-
-    errcode = clSetKernelArg(kernel, 0, sizeof(cl_mem), &memC[i]);
-    checkErr(errcode, "clSetKernelArg0");
-
-    errcode = clSetKernelArg(kernel, 1, sizeof(cl_mem), &memA);
-    checkErr(errcode, "clSetKernelArg1");
-
-    errcode = clSetKernelArg(kernel, 2, sizeof(cl_mem), &memB);
-    checkErr(errcode, "clSetKernelArg2");
-
-    errcode = clSetKernelArg(kernel, 3, sizeof(cl_int), &m);
-    checkErr(errcode, "clSetKernelArg3");
-
-    errcode = clSetKernelArg(kernel, 4, sizeof(cl_int), &k);
-    checkErr(errcode, "clSetKernelArg4");
-
-    errcode = clSetKernelArg(kernel, 5, sizeof(cl_int), &n);
-    checkErr(errcode, "clSetKernelArg5");
-
-    errcode = clSetKernelArg(kernel, 6, sizeof(cl_float), &alpha);
-    checkErr(errcode, "clSetKernelArg6");
-
-    errcode = clSetKernelArg(kernel, 7, sizeof(cl_float), &beta);
-    checkErr(errcode, "clSetKernelArg7");
+  
+    checkErr(clSetKernelArg(kernel, 0, sizeof(cl_int), &nota), "clSetKernelArg0");
+    checkErr(clSetKernelArg(kernel, 1, sizeof(cl_int), &notb), "clSetKernelArg1");
+    checkErr(clSetKernelArg(kernel, 2, sizeof(cl_int), &m), "clSetKernelArg2");
+    checkErr(clSetKernelArg(kernel, 3, sizeof(cl_int), &n), "clSetKernelArg3");
+    checkErr(clSetKernelArg(kernel, 4, sizeof(cl_int), &k), "clSetKernelArg4");
+    checkErr(clSetKernelArg(kernel, 5, sizeof(cl_float), &alpha), "clSetKernelArg5");
+    checkErr(clSetKernelArg(kernel, 6, sizeof(cl_mem), &memA), "clSetKernelArg6");
+    checkErr(clSetKernelArg(kernel, 7, sizeof(cl_int), &lda), "clSetKernelArg7");
+    checkErr(clSetKernelArg(kernel, 8, sizeof(cl_mem), &memB), "clSetKernelArg8");
+    checkErr(clSetKernelArg(kernel, 9, sizeof(cl_int), &ldb), "clSetKernelArg9");
+    checkErr(clSetKernelArg(kernel, 10, sizeof(cl_float), &beta), "clSetKernelArg10");
+    checkErr(clSetKernelArg(kernel, 11, sizeof(cl_mem), &memC[i]), "clSetKernelArg11");
+    checkErr(clSetKernelArg(kernel, 12, sizeof(cl_int), &ldc), "clSetKernelArg12");
 
     errcode = clEnqueueNDRangeKernel(command_queues[i], kernel, 2, NULL, global_work_size, local_work_size, 0, NULL, NULL);
     checkErr(errcode, "clEnqueueNDRangeKernel");
@@ -158,9 +148,12 @@ int opencl_operation(cl_char TransA, cl_char TransB, cl_int m, cl_int n, cl_int 
 void blas_sgemm(cl_char transa, cl_char transb, cl_int m, cl_int  n,  cl_int  k,
                 cl_float alpha, cl_float *a, cl_int lda, cl_float *b, cl_int ldb,
                 cl_float beta, cl_float *c, cl_int ldc, unsigned int flags) {
-  int root_argument, mpi_size, spawns_m;
+
+  int root_argument, mpi_size, spawns_m, nota, notb;
   MPI_Comm intercomm, parent;
 
+  nota = transa == 'N' || transa == 'n';
+  notb = transb == 'N' || transb == 'n';
   if(flags & USE_MPI) {
     char* universe_size = getenv("MPI_UNIVERSE_SIZE");
     if(universe_size == NULL) {
@@ -181,8 +174,6 @@ void blas_sgemm(cl_char transa, cl_char transb, cl_int m, cl_int  n,  cl_int  k,
       intercomm = parent;
       root_argument = 0;
     }
-  } else {
-    root_argument = 1;
   }
 
   if(flags & USE_MPI) {
@@ -193,7 +184,8 @@ void blas_sgemm(cl_char transa, cl_char transb, cl_int m, cl_int  n,  cl_int  k,
     MPI_Bcast(&alpha, 1, MPI_FLOAT, root_argument, intercomm);
     MPI_Bcast(&beta, 1, MPI_FLOAT, root_argument, intercomm);
     MPI_Bcast(&flags, 1, MPI_UNSIGNED, root_argument, intercomm);
-    MPI_Bcast(&mpi_size, 1, MPI_INTEGER, root_argument, intercomm);
+    MPI_Bcast(&nota, 1, MPI_INTEGER, root_argument, intercomm);
+    MPI_Bcast(&notb, 1, MPI_INTEGER, root_argument, intercomm);
 
     if(parent != MPI_COMM_NULL) {
       m = spawns_m;
@@ -213,7 +205,7 @@ void blas_sgemm(cl_char transa, cl_char transb, cl_int m, cl_int  n,  cl_int  k,
     }
   }
 
-  opencl_operation(transa, transb, m, n, k, alpha, a, b, beta, c, flags, "blas_sgemm");
+  opencl_operation(nota, notb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc, flags, "blas_sgemm");
 
   if(flags & USE_MPI) {
     // Recv & Send C
--- operations.cl
+++ operations.cl
@@ -1,9 +1,9 @@
 // Thread block size
 #define BLOCK_SIZE 16
 
-__kernel void blas_sgemm(__global float *C, __global const float *A, __global const float *B, const uint rowsA, const uint colsA, const uint colsB,
-                        float alpha, float beta) {
-  uint rowsB = colsA;
+__kernel void blas_sgemm(int nota, int notb, int m, int n, int k, float alpha, __global const float *a, int lda,
+                        __global const float *b, int ldb, float beta, __global float *c, int ldc) {
+
   uint ra, ca, rb, cb;
   float Csub = 0;
 
@@ -15,50 +15,48 @@ __kernel void blas_sgemm(__global float *C, __global const float *A, __global co
   int tx = get_local_id(0);
   int ty = get_local_id(1);
 
-  // Index of the first sub-matrix of A processed by the block
-  int a = BLOCK_SIZE * bx;
+  // Index of the first sub-matrix of a processed by the block
+  int indexa = BLOCK_SIZE * bx;
  
-  // Index of the first sub-matrix of B processed by the block
-  int b = BLOCK_SIZE * by;
+  // Index of the first sub-matrix of b processed by the block
+  int indexb = BLOCK_SIZE * by;
 
   // Declaration of the local memory array As 
-  // used to store the sub-matrix of A
+  // used to store the sub-matrix of a
   __local float As[BLOCK_SIZE][BLOCK_SIZE];
                            
   // Declaration of the local memory array Bs 
-  // used to store the sub-matrix of B
+  // used to store the sub-matrix of b
   __local float Bs[BLOCK_SIZE][BLOCK_SIZE];
 
-  for(int i=0; i<colsA; i+=BLOCK_SIZE) {
+  for(int i=0; i<k; i+=BLOCK_SIZE) {
     // Load the matrices from global memory to local memory;
     // each thread loads one element of each matrix
     // Barriers are used for synchronization and to be sure we don't
-    // overwrite an address that's going to be used
+    // overwrite an address that is going to be used
     ra = tx+BLOCK_SIZE*bx;
     ca = i+ty;
     rb = i+tx;
     cb = ty+BLOCK_SIZE*by;
     barrier(CLK_LOCAL_MEM_FENCE);
-    if(ra >= rowsA || ca >= colsA)
+    if(ra >= m || ca >= k)
       As[tx][ty] = 0;
     else
-      As[tx][ty] = A[ra*colsA+ca];
-    if(rb >= rowsB || cb >= colsB)
+      As[tx][ty] = a[ra*k+ca];
+    if(rb >= k || cb >= n)
       Bs[tx][ty] = 0;
     else
-      Bs[tx][ty] = B[rb*colsB+cb];
+      Bs[tx][ty] = b[rb*n+cb];
     barrier(CLK_LOCAL_MEM_FENCE);
 
-    for(int k=0; k<BLOCK_SIZE; k++)
-      Csub += As[tx][k] * Bs[k][ty];
+    for(int l=0; l<BLOCK_SIZE; l++)
+      Csub += As[tx][l] * Bs[l][ty];
   }
-
-  if(tx+a < rowsA && ty+b < colsB) { // In bounds
-    if(beta) {
-      C[(tx+a)*colsB+(ty+b)] = alpha*Csub + beta*C[(tx+a)*colsB+(ty+b)];
-    } else {
-      C[(tx+a)*colsB+(ty+b)] = alpha*Csub;
-    }
+  if(tx+indexa < m && ty+indexb < n) { // In bounds
+    if(beta)
+      c[(tx+indexa)*n+(ty+indexb)] = alpha*Csub + beta*c[(tx+indexa)*n+(ty+indexb)];
+    else
+      c[(tx+indexa)*n+(ty+indexb)] = alpha*Csub;
   }
 }
 
