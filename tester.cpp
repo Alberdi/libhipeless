4500517a50aa7e0ada2099cbfd1990af9b2e92b6
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2013-11-19 10:20:31 +0100

    Every instance of float is replaced by number.

--- tester.cpp
+++ tester.cpp
@@ -83,69 +83,69 @@ static const char* test_tester(int flags, number t) {
 }
 
 template <typename number>
-static const char* test_sgemm_ones(int flags, number t) {
+static const char* test_xgemm_ones(int flags, number t) {
   number *a, *b, *c, *d;
   load_file("tests/xgemm_ones.txt", &a, &b, &c);
 
   d = (number*) malloc(32*32*sizeof(number));
   
   // D == C
-  blas_sgemm('N', 'N', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_ones(0).", equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('N', 'N', 32, 32, 32, (number)(number)1, a, 32, b, 32, (number)(number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_ones(0).", equal_matrices(32, 32, d, 32, c, 32));
 
   // D == C when op(A) == A', (A is symmetric).
-  blas_sgemm('T', 'N', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_ones(1).", equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('T', 'N', 32, 32, 32, (number)(number)1, a, 32, b, 32, (number)(number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_ones(1).", equal_matrices(32, 32, d, 32, c, 32));
 
   // D == C when op(A) == A', (A is symmetric).
-  blas_sgemm('T', 'N', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_ones(2).", equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('T', 'N', 32, 32, 32, (number)(number)1, a, 32, b, 32, (number)(number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_ones(2).", equal_matrices(32, 32, d, 32, c, 32));
 
   // D == C when op(B) == B' (B is symmetric).
-  blas_sgemm('N', 'T', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_ones(3).", equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('N', 'T', 32, 32, 32, (number)(number)1, a, 32, b, 32, (number)(number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_ones(3).", equal_matrices(32, 32, d, 32, c, 32));
   
   // D == C when op(B) == B', op(A) == A' (A, B are symmetric).
-  blas_sgemm('T', 'T', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_ones(4).", equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('T', 'T', 32, 32, 32, (number)(number)1, a, 32, b, 32, (number)(number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_ones(4).", equal_matrices(32, 32, d, 32, c, 32));
 
   free(a); free(b); free(c); free(d);
   return 0;
 }
 
 template <typename number>
-static const char* test_sgemm_rand(int flags, number t) {
+static const char* test_xgemm_rand(int flags, number t) {
   number *a, *b, *c, *d;
   load_file("tests/xgemm_rand.txt", &a, &b, &c);
 
   d = (number*) malloc(32*32*sizeof(number));
 
   // D == C
-  blas_sgemm('N', 'N', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_rand(0).", equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('N', 'N', 32, 32, 32, (number)1, a, 32, b, 32, (number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_rand(0).", equal_matrices(32, 32, d, 32, c, 32));
 
   // D != C when op(A) == A', (A is not symmetric).
-  blas_sgemm('T', 'N', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_rand(1).", !equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('T', 'N', 32, 32, 32, (number)1, a, 32, b, 32, (number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_rand(1).", !equal_matrices(32, 32, d, 32, c, 32));
 
   // D != C when op(A) == A', (A is not symmetric).
-  blas_sgemm('T', 'N', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_rand(2).", !equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('T', 'N', 32, 32, 32, (number)1, a, 32, b, 32, (number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_rand(2).", !equal_matrices(32, 32, d, 32, c, 32));
 
   // D != C when op(B) == B' (B is not symmetric).
-  blas_sgemm('N', 'T', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_rand(3).", !equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('N', 'T', 32, 32, 32, (number)1, a, 32, b, 32, (number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_rand(3).", !equal_matrices(32, 32, d, 32, c, 32));
   
   // D != C when op(B) == B', op(A) == A' (A, B are not symmetric).
-  blas_sgemm('T', 'T', 32, 32, 32, 1, a, 32, b, 32, 0, d, 32, flags);
-  mu_assert("Error in test_sgemm_rand(4).", !equal_matrices(32, 32, d, 32, c, 32));
+  blas_xgemm('T', 'T', 32, 32, 32, (number)1, a, 32, b, 32, (number)0, d, 32, flags);
+  mu_assert("Error in test_xgemm_rand(4).", !equal_matrices(32, 32, d, 32, c, 32));
 
   free(a); free(b); free(c); free(d);
   return 0;
 }
 
 template <typename number>
-static const char* test_sgemm_rand_big(int flags, number t) {
+static const char* test_xgemm_rand_big(int flags, number t) {
   number *a, *b, *c, *d;
   // A = 331x137; B = 137x401; C = 331x401;
   load_file("tests/xgemm_rand_big.txt", &a, &b, &c);
@@ -153,23 +153,23 @@ static const char* test_sgemm_rand_big(int flags, number t) {
   d = (number*) malloc(331*405*sizeof(number));
 
   // D == C
-  blas_sgemm('N', 'N', 331, 401, 137, 1, a, 137, b, 401, 0, d, 405, flags);
-  mu_assert("Error in test_sgemm_rand_big(0).", equal_matrices(331, 401, d, 405, c, 401));
+  blas_xgemm('N', 'N', 331, 401, 137, (number)1, a, 137, b, 401, (number)0, d, 405, flags);
+  mu_assert("Error in test_xgemm_rand_big(0).", equal_matrices(331, 401, d, 405, c, 401));
 
   // D == C (first 15 rows)
-  blas_sgemm('N', 'N', 15, 401, 137, 1, a, 137, b, 401, 0, d, 405, flags);
-  mu_assert("Error in test_sgemm_rand_big(1).", equal_matrices(15, 401, d, 405, c, 401));
+  blas_xgemm('N', 'N', 15, 401, 137, (number)1, a, 137, b, 401, (number)0, d, 405, flags);
+  mu_assert("Error in test_xgemm_rand_big(1).", equal_matrices(15, 401, d, 405, c, 401));
 
   // D != C (first 15 rows, using 15 columns of C)
-  blas_sgemm('N', 'N', 4, 4, 137, 1, a, 137, b, 401, 0, d, 405, flags);
-  mu_assert("Error in test_sgemm_rand_big(2).", equal_matrices(15, 15, d, 405, c, 401));
+  blas_xgemm('N', 'N', 4, 4, 137, (number)1, a, 137, b, 401, (number)0, d, 405, flags);
+  mu_assert("Error in test_xgemm_rand_big(2).", equal_matrices(15, 15, d, 405, c, 401));
 
   free(a); free(b); free(c); free(d);
   return 0;
 }
 
 template <typename number>
-static const char* test_sgemm_rand_big_alphabeta(int flags, number t) {
+static const char* test_xgemm_rand_big_alphabeta(int flags, number t) {
   number *a, *b, *c, *d;
   int i, j;
   // A = 331x137; B = 137x401; C = 331x401;
@@ -184,73 +184,73 @@ static const char* test_sgemm_rand_big_alphabeta(int flags, number t) {
   }
 
   // D == C (beta = -2)
-  blas_sgemm('N', 'N', 331, 401, 137, 12.345, a, 137, b, 401, -2.1, d, 401, flags);
-  mu_assert("Error in test_sgemm_rand_big_alphabeta(0).", equal_matrices(331, 401, d, 401, c, 401));
+  blas_xgemm('N', 'N', 331, 401, 137, (number)12.345, a, 137, b, 401, (number)-2.1, d, 401, flags);
+  mu_assert("Error in test_xgemm_rand_big_alphabeta(0).", equal_matrices(331, 401, d, 401, c, 401));
 
   free(a); free(b); free(c); free(d);
   return 0;
 }
 
 template <typename number>
-static const char* test_sgemm_row(int flags, number t) {
+static const char* test_xgemm_row(int flags, number t) {
   number *a, *b, *c, *d;
   load_file("tests/xgemm_row.txt", &a, &b, &c);
 
   d = (number*) malloc(1*1*sizeof(number));
 
   // D == C
-  blas_sgemm('N', 'N', 1, 1, 128, 1, a, 128, b, 1, 0, d, 1, flags);
-  mu_assert("Error in test_sgemm_row(0).", equal_matrices(1, 1, d, 1, c, 1));
+  blas_xgemm('N', 'N', 1, 1, 128, (number)1, a, 128, b, 1, (number)0, d, 1, flags);
+  mu_assert("Error in test_xgemm_row(0).", equal_matrices(1, 1, d, 1, c, 1));
 
   // D == 12.34*C (alpha = 12.34)
-  blas_sgemm('N', 'N', 1, 1, 128, 12.34, a, 128, b, 1, 0, d, 1, flags);
+  blas_xgemm('N', 'N', 1, 1, 128, (number)12.34, a, 128, b, 1, (number)0, d, 1, flags);
   d[0] /= 12.34;
-  mu_assert("Error in test_sgemm_row(1).", equal_matrices(1, 1, d, 1, c, 1));
+  mu_assert("Error in test_xgemm_row(1).", equal_matrices(1, 1, d, 1, c, 1));
 
   // We'll store in D the calculation of the first 4 multiplications.
-  blas_sgemm('N', 'N', 1, 1, 4, 1, a, 128, b, 1, 0, d, 1, flags);
+  blas_xgemm('N', 'N', 1, 1, 4, (number)1, a, 128, b, 1, (number)0, d, 1, flags);
   // And now we replicate it in C.
   c[0] = a[0]*b[0] + a[1]*b[1] + a[2]*b[2] + a[3]*b[3];
-  mu_assert("Error in test_sgemm_row(2).", equal_matrices(1, 1, d, 1, c, 1));
+  mu_assert("Error in test_xgemm_row(2).", equal_matrices(1, 1, d, 1, c, 1));
 
   // Now, we'll use pointers to calculate the following 4 multiplications.
-  blas_sgemm('N', 'N', 1, 1, 4, 1, &a[4], 124, &b[4], 1, 0, d, 1, flags);
+  blas_xgemm('N', 'N', 1, 1, 4, (number)1, &a[4], 124, &b[4], 1, (number)0, d, 1, flags);
   // And now we replicate it in C.
   c[0] = a[4]*b[4] + a[5]*b[5] + a[6]*b[6] + a[7]*b[7];
-  mu_assert("Error in test_sgemm_row(3).", equal_matrices(1, 1, d, 1, c, 1));
+  mu_assert("Error in test_xgemm_row(3).", equal_matrices(1, 1, d, 1, c, 1));
 
   // Do the same with alpha = 1.2 and beta = 2.3
-  blas_sgemm('N', 'N', 1, 1, 4, 1.2, &a[4], 124, &b[4], 1, 2.3, d, 1, flags);
+  blas_xgemm('N', 'N', 1, 1, 4, (number)1.2, &a[4], 124, &b[4], 1, (number)2.3, d, 1, flags);
   // And now we replicate it in C.
   // C = 1.2*C + 2.3*C = 3.5*C
   c[0] = 3.5*c[0];
-  mu_assert("Error in test_sgemm_row(4).", equal_matrices(1, 1, d, 1, c, 1));
+  mu_assert("Error in test_xgemm_row(4).", equal_matrices(1, 1, d, 1, c, 1));
 
   free(a); free(b); free(c); free(d);
   return 0;
 }
 
 template <typename number>
-static const char* test_sgemm_row_trans(int flags, number t) {
+static const char* test_xgemm_row_trans(int flags, number t) {
   number *a, *b, *c, *d;
   load_file("tests/xgemm_row_trans.txt", &a, &b, &c);
 
   d = (number*) malloc(128*128*sizeof(number));
 
   // D == C
-  blas_sgemm('T', 'T', 128, 128, 1, 1, a, 128, b, 1, 0, d, 128, flags);
-  mu_assert("Error in test_sgemm_row_trans(0).", equal_matrices(128, 128, d, 128, c, 128));
+  blas_xgemm('T', 'T', 128, 128, 1, (number)1, a, 128, b, 1, (number)0, d, 128, flags);
+  mu_assert("Error in test_xgemm_row_trans(0).", equal_matrices(128, 128, d, 128, c, 128));
 
   // D[0:3] == C[0:3]
-  blas_sgemm('T', 'T', 4, 4, 1, 1, a, 128, b, 1, 0, d, 128, flags);
-  mu_assert("Error in test_sgemm_row_trans(1).", equal_matrices(4, 4, d, 128, c, 128));
+  blas_xgemm('T', 'T', 4, 4, 1, (number)1, a, 128, b, 1, (number)0, d, 128, flags);
+  mu_assert("Error in test_xgemm_row_trans(1).", equal_matrices(4, 4, d, 128, c, 128));
 
   // Let's be sure we're not overflowing d when calculating a fraction.
   free(d);
   d = (number*) malloc(35*35*sizeof(number));
   // D == C (35 rows, 35 columns).
-  blas_sgemm('T', 'T', 35, 35, 1, 1, a, 128, b, 1, 0, d, 35, flags);
-  mu_assert("Error in test_sgemm_row_trans(2).", equal_matrices(35, 35, d, 35, c, 128));
+  blas_xgemm('T', 'T', 35, 35, 1, (number)1, a, 128, b, 1, (number)0, d, 35, flags);
+  mu_assert("Error in test_xgemm_row_trans(2).", equal_matrices(35, 35, d, 35, c, 128));
 
   free(a); free(b); free(c); free(d);
   return 0;
@@ -267,12 +267,12 @@ static const char* all_tests(number t) {
   for(i = 0; i < 4; i++) {
     tests_run = 0;
     printf("Using flags = 0x%x.\n", flags[i], t);
-    mu_run_test(test_sgemm_ones, flags[i], t);
-    mu_run_test(test_sgemm_rand, flags[i], t);
-    mu_run_test(test_sgemm_rand_big, flags[i], t);
-    mu_run_test(test_sgemm_rand_big_alphabeta, flags[i], t);
-    mu_run_test(test_sgemm_row, flags[i], t);
-    mu_run_test(test_sgemm_row_trans, flags[i], t);
+    mu_run_test(test_xgemm_ones, flags[i], t);
+    mu_run_test(test_xgemm_rand, flags[i], t);
+    mu_run_test(test_xgemm_rand_big, flags[i], t);
+    mu_run_test(test_xgemm_rand_big_alphabeta, flags[i], t);
+    mu_run_test(test_xgemm_row, flags[i], t);
+    mu_run_test(test_xgemm_row_trans, flags[i], t);
   }
   return 0;
 }
