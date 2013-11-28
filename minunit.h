e1e3858d94248a96543abf43398f7c29085f736f
Marcelino Alberdi <marcelino.alberdi@gmail.com>
2013-11-28 12:04:29 +0100

    Forces writing to stdout after each test.

--- minunit.h
+++ minunit.h
@@ -1,5 +1,5 @@
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test, flags, type) do { const char *message = test(flags, type); \
                                        if (message) return message; \
-                                       else printf("Ran test %i.\n", ++tests_run); } while (0)
+                                       else printf("Ran test %i.\n", ++tests_run); fflush(stdout); } while (0)
 extern int tests_run;
